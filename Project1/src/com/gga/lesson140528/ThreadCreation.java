b597314c16f08db46b49a7f65246df92ed6949ac
student <student@stud-212.itcwin.com>
2014-05-28 21:12:35 +0400

    add lesson 28 may

---
+++ Project1/src/com/gga/lesson140528/ThreadCreation.java
@@ -0,0 +1,31 @@
+package com.gga.lesson140528;
+
+public class ThreadCreation {
+	static class MyTask extends Thread {
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				Utils.pause(1000);
+				System.out.println(Thread.currentThread().getName() +" : i = " + i);
+			}
+		}
+	}
+	
+	static class MyTask2 implements Runnable {
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 10; i++) {
+				Utils.pause(1000);
+				System.out.println(Thread.currentThread().getName() +" : i = " + i);
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		new MyTask().start();
+		new Thread(new MyTask2()).start();
+		System.out.println(Thread.activeCount());
+	}
+}
---
+++ Project1/src/com/gga/lesson140528/Utils.java
@@ -0,0 +1,12 @@
+package com.gga.lesson140528;
+
+public class Utils {
+
+	public static void pause(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
